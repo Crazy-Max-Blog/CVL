9ed5a28d1a9ed0663e72a3b3af1677415210d394
ser-reb <99679566+ser-reb@users.noreply.github.com>
2023-10-08 16:51:15 +0400

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -11,25 +11,8 @@ root["bg"] = "SteelBlue1"
 w, h = 500, 500
 root.geometry(f'{w}x{h}')    
 
-def r():
-    for widget in root.winfo_children():
-        widget.destroy()
-    
-    l = Button(font='Times 15',fg="white",activebackground="SteelBlue1",bd=0,bg="SteelBlue1", text="Загрузить задачу", command=load_task)
-    l.pack()
-    c = Button(fg="white",activebackground="SteelBlue1",bd=0,bg="SteelBlue1", text="выход", command=exit)
-    c.pack(side=RIGHT, anchor=SE)
-
-def load_task():
-    p=filedialog.askopenfilename(filetypes=(("Задачи CVL", "*.cvl"),("Задачи ВЛ", "*.edu1")))
-    print("load task", p)
-    w1(p)
-
-def exit():
-    print("exit")
-    root.destroy()
-    
-print(rd("1.cvl"))
-r()
+w = window(root)
+print(w.rd("1.cvl"))
+w.r()
 
 root.mainloop()
--- r.py
+++ r.py
@@ -1,23 +1,48 @@
-def rd(f):
-    with open(f, "r", encoding="UTF-8") as file:
-        s = file.read()
-    l1=s.split('\n')
-    print(len(l1))
-    match len(l1):
-        case 2:
-            return [l1[0],l1[1],"",""]
-        case 4:
-            return [l1[0],l1[1],l1[2].split(),l1[3].split()]
-    print("error: error file") 
-    #return
+from tkinter import *
+from tkinter import filedialog
+
+class window:
+    def __init__(self, r):
+        self.root = r
+        
+    def r(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        
+        self.l=Button(font='Times 15',fg="white",activebackground=self.root["bg"],bd=0,bg=self.root["bg"],text="Загрузить задачу",command=self.load_task)
+        self.l.pack()
+        self.c = Button(fg="white",activebackground=self.root["bg"],bd=0,bg=self.root["bg"], text="выход", command=exit)
+        self.c.pack(side=RIGHT, anchor=SE)
     
-def w1(p):
-    for widget in root.winfo_children():
-        widget.destroy()
+    def load_task(self):
+        self.p=filedialog.askopenfilename(filetypes=(("Задачи CVL", "*.cvl"),("Задачи ВЛ", "*.edu1")))
+        print("load task", self.p)
+        self.w1(self.p)
     
-    print(rd(p))
-    
-    print(open(p,'r').read())
-    Label(text=rd(p),bg="SteelBlue1").pack()
-    l = Button(fg="white",activebackground="SteelBlue1",bd=0,bg="SteelBlue1", text="назад", command=r)
-    l.pack(side=LEFT, anchor=SW)    
+    def exit(self):
+        print("exit")
+        self.root.destroy()
+        
+    def rd(self, f):
+        with open(f, "r", encoding="UTF-8") as file:
+            self.s = file.read()
+        self.l1=self.s.split('\n')
+        print(len(self.l1))
+        match len(self.l1):
+            case 2:
+                return [self.l1[0],self.l1[1],"",""]
+            case 4:
+                return [self.l1[0],self.l1[1],self.l1[2].split(),self.l1[3].split()]
+        print("error: error file") 
+        #return
+        
+    def w1(self, p):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        
+        print(self.rd(p))
+        
+        print(open(p,'r').read())
+        Label(text=self.rd(p),bg="SteelBlue1").pack()
+        self.l = Button(fg="white",activebackground="SteelBlue1",bd=0,bg="SteelBlue1", text="назад", command=self.r)
+        self.l.pack(side=LEFT, anchor=SW)    
